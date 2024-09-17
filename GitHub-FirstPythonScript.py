86c3066331e501464c394c8c28f225f0154630b5
nishadiniwidanagamage <nishadini@ksu.edu>
2024-09-17 16:16:53 -0500

    Update GitHub-FirstPythonScript.py
    Calculating the total stream length within a 10 km buffered region in Flint Hills ecoregion in miles.

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -11,6 +11,7 @@
 
 # Import arcpy module and allow overwrites
 import arcpy
+import arcpy.management
 arcpy.env.overwriteOutput = True
 
 # Set current workspace
@@ -24,12 +25,16 @@ select_ecoregion = arcpy.management.SelectLayerByAttribute('ks_ecoregions', 'NEW
 ecoregion_buffered = arcpy.analysis.Buffer(select_ecoregion, "Flinthills_buffered", "10 Kilometers")
 
 # Clipping the major rivers in Kansas with buffered ecoregion
-clipped_major_rivers = arcpy.analysis.Clip('ks_major_rivers', ecoregion_buffered, "Clippes_ks_major_rivers")
+clipped_major_rivers = arcpy.analysis.Clip('ks_major_rivers', ecoregion_buffered, "clipped_ks_major_rivers")
 
 # Saving clipped rivers into a feature class
-Clipped_rivers_feature_class = arcpy.management.CopyFeatures(clipped_major_rivers, "Clipped_rivers")
+clipped_rivers = arcpy.management.CopyFeatures(clipped_major_rivers, "Clipped_rivers")
 
-# Calculating the total stream length of clipped rivers feature class in meters
-total_stream_length = arcpy.analysis.Statistics(Clipped_rivers_feature_class, "total_stream_length", [["Shape_Length", "SUM"]])
+# Calculating the clipped stream lengths in miles
+stream_length_miles = arcpy.management.AddGeometryAttributes(clipped_rivers, 'LENGTH', 'MILES_US')
+
+# Calculating the total stream length of clipped rivers feature class
+total_stream_length = arcpy.analysis.Statistics(stream_length_miles, "total_stream_length", [["LENGTH", "SUM"]])
+
 
 
