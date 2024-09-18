7a611288ca08bb97ad1f79724425bcc27acc3b6b
Shawn Hutchinson <shutch@ksu.edu>
2024-09-18 09:54:18 -0500

    Instructor Corrections
    The original script ran to completion and generated a correct result.
    
    You do not need to import "arcpy.management" as it is included with "arcpy".  I also removed one unnecessary function (Copy Features) and several result objects that weren't needed.  Otherwise a compact and functional solution.

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -11,7 +11,6 @@
 
 # Import arcpy module and allow overwrites
 import arcpy
-import arcpy.management
 arcpy.env.overwriteOutput = True
 
 # Set current workspace
@@ -22,19 +21,13 @@ arcpy.env.workspace = r"C:\Users\nishadini\OneDrive - Kansas State University\Do
 select_ecoregion = arcpy.management.SelectLayerByAttribute('ks_ecoregions', 'NEW_SELECTION', "US_L3NAME = 'Flint Hills'")
 
 # Creating a 10 km buffer zone around the selected ecoregion
-ecoregion_buffered = arcpy.analysis.Buffer(select_ecoregion, "Flinthills_buffered", "10 Kilometers")
+arcpy.analysis.Buffer(select_ecoregion, "Flinthills_buffered", "10 Kilometers")
 
 # Clipping the major rivers in Kansas with buffered ecoregion
-clipped_major_rivers = arcpy.analysis.Clip('ks_major_rivers', ecoregion_buffered, "clipped_ks_major_rivers")
-
-# Saving clipped rivers into a feature class
-clipped_rivers = arcpy.management.CopyFeatures(clipped_major_rivers, "Clipped_rivers")
+arcpy.analysis.Clip('ks_major_rivers', "Flinthills_buffered", "clipped_ks_major_rivers")
 
 # Calculating the clipped stream lengths in miles
-stream_length_miles = arcpy.management.AddGeometryAttributes(clipped_rivers, 'LENGTH', 'MILES_US')
+arcpy.management.AddGeometryAttributes("clipped_ks_major_rivers", 'LENGTH', 'MILES_US')
 
 # Calculating the total stream length of clipped rivers feature class
-total_stream_length = arcpy.analysis.Statistics(stream_length_miles, "total_stream_length", [["LENGTH", "SUM"]])
-
-
-
+arcpy.analysis.Statistics("clipped_ks_major_rivers", "total_stream_length", [["LENGTH", "SUM"]])
