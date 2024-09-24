c3dc4d7e49c65c6f1d1f292224c2d1119a749465
nishadiniwidanagamage <nishadini@ksu.edu>
2024-09-24 10:00:44 -0500

    Update GitHub-FirstPythonScript.py

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -16,18 +16,23 @@ arcpy.env.overwriteOutput = True
 # Set current workspace
 arcpy.env.workspace = r"C:\Users\nishadini\OneDrive - Kansas State University\Documents\GitHub\firstpythonscript-nishadiniwidanagamage\GISproject\ExerciseData.gdb" 
 
+# Introduce variables
+ecoregions = "ks_ecoregions"
+selected_ecoregion = "Flint Hills"
+rivers = "ks_major_rivers"
+
 # Perform geoprocessing
 # Selecting Flint Hills ecoregion
-select_ecoregion = arcpy.management.SelectLayerByAttribute('ks_ecoregions', 'NEW_SELECTION', "US_L3NAME = 'Flint Hills'")
+select_ecoregion = arcpy.management.SelectLayerByAttribute(ecoregions, 'NEW_SELECTION', "US_L3NAME = 'Flint Hills'")
 
 # Creating a 10 km buffer zone around the selected ecoregion
-arcpy.analysis.Buffer(select_ecoregion, "Flinthills_buffered", "10 Kilometers")
+arcpy.analysis.Buffer(select_ecoregion, "ecoregion_buffered", "10 Kilometers")
 
 # Clipping the major rivers in Kansas with buffered ecoregion
-arcpy.analysis.Clip('ks_major_rivers', "Flinthills_buffered", "clipped_ks_major_rivers")
+arcpy.analysis.Clip(rivers, "ecoregion_buffered", "clipped_major_rivers")
 
 # Calculating the clipped stream lengths in miles
-arcpy.management.AddGeometryAttributes("clipped_ks_major_rivers", 'LENGTH', 'MILES_US')
+arcpy.management.AddGeometryAttributes("clipped_major_rivers", 'LENGTH', 'MILES_US')
 
 # Calculating the total stream length of clipped rivers feature class
-arcpy.analysis.Statistics("clipped_ks_major_rivers", "total_stream_length", [["LENGTH", "SUM"]])
+arcpy.analysis.Statistics("clipped_major_rivers", "total_stream_length", [["LENGTH", "SUM"]])
